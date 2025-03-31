9c5cf9a7b20b8c5a5236c13ceb34d41482f44a3e
VickersCode <tyler31vickers@gmail.com>
2025-03-31 09:29:46 -0700

    add switch statement

--- functions.cpp
+++ functions.cpp
@@ -62,6 +62,51 @@ void processOperation(const std::string &opcode, const std::vector<std::string> 
     if (operands.size() == 3) {
         op3 = std::stoul(operands[2], nullptr, 16);
     }
+
+    switch (opcodeVal) {
+        case 1: // ADD
+            
+            break;
+        case 2: // SUB
+            
+            break;
+        case 3: // CMP
+            
+            break;
+        case 4: // MOV
+            
+            break;
+        case 5: // AND
+            
+            break;
+        case 6: // OR
+
+            break;
+        case 7: // XOR
+            
+            break;
+        case 8: // LOAD
+            
+            break;
+        case 9: // STORE
+            
+            break;
+        case 10: // BAL
+            
+            break;
+        case 11: // BEQ
+            
+            break;
+        case 12: // BNE
+            
+            break;
+        default:
+            output += "Unsupported Operation";
+            isValid = false;
+            break;
+    }
+
+    std::cout << output << std::endl;
 }
 
 std::string toHexString(uint32_t value) {
